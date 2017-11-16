7c0bb81f26b533e35f86331d287f7ea083eceb71
Dejan Mijatovic <mjtvc93@gmail.com>
2017-11-16 23:58:11 +0100

    Changed the rgb color picker to hsl in js

--- script.js
+++ script.js
@@ -8,10 +8,8 @@ function createGrid(gridSpaces) {
 }
 
 function addBackgroundColor(element) {
-  let random1 = Math.floor(Math.random() * 255);
-  let random2 = Math.floor(Math.random() * 255);
-  let random3 = Math.floor(Math.random() * 255);
-  let randomColor = "rgb(" + random1 + ", " + random2 + ", " + random3 + ")";
+  let random = Math.floor(Math.random() * 359);
+  let randomColor = "hsl(" + random + ", 100%, 50%)";
   element.addEventListener("mouseover", event => {
     element.style.backgroundColor = randomColor;
   });
